84749968fed08dd8c8f32d082c7dc4081b0798db
amhatami <am.hatami@gmail.com>
2018-10-08 09:11:44 -0700

    Initial Commit

---
+++ IOS-Swift-UITableViewXib/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IOS-Swift-UITableViewXib
+//
+//  Created by Pooya on 2018-10-08.
+//  Copyright © 2018 Pooya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
