43a6fd7a9be3abde0e08a63610b8c84596a76a4f
Pooya <am.hatami@gmail.com>
2018-11-02 10:17:59 -0700

    done

---
+++ IOS-Swift-UITableViewXib/TableViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TableViewController.swift
+//  IOS-Swift-UITableViewXib
+//
+//  Created by Pooya on 2018-10-08.
+//  Copyright © 2018 Pooya. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
